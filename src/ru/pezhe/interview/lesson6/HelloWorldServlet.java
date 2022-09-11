b76c050383342caaaeeb58c82ee9fb1d8e88d255
pezhe <pezhe@list.ru>
2022-09-11 22:41:56 +0300

    lesson 6 hw done

---
+++ src/ru/pezhe/interview/lesson6/HelloWorldServlet.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<html>");
+        printWriter.print("<body>");
+        printWriter.print("<h1>Hello World!</h1>");
+        printWriter.print("</body>");
+        printWriter.print("</html>");
+        printWriter.close();
+    }
+
+}
